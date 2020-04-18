d9c9a85c3cc94c8b8085130837226428202ab152
JoseTobias <tobiassouza0@gmail.com>
2020-04-18 11:16:23 -0300

    layout

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,11 +1,41 @@
 <template>
   <div class="board">
+    <div class="flex flex-row items-start">
+      <div class="column"
+        v-for="(column, index) of board.columns"
+        :key="index"
+      >
+        <div class="flex items-center mb-2 font-bold">
+          {{ column.name }}
+        </div>
+        <div class="list-reset">
+          <div class="task"
+            v-for="task of column.tasks"
+            :key="task.id"
+          >
+            <span
+              class="w-full flex-no-shink font-bold"
+            >
+              {{ task.name }}
+            </span>
+            <p
+              class="w-full flex-no-shink mt-1 text-sm"
+              v-if="task.description"
+            >
+              {{ task.description }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 
 export default {
+  computed: mapState(['board'])
 }
 </script>
 
